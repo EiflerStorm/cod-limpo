8582711316e6a121b48ee269991914ec7fe768da
Eduardo Eifler <126223493+EiflerStorm@users.noreply.github.com>
2024-04-27 19:18:22 -0300

    Update1 questao.c
    Adição de comentários para melhor compreensão de métodos e variáveis no código.

--- questao.c
+++ questao.c
@@ -4,9 +4,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-
+//Variavel global número de processo filhos
 int nfilho;
 
+//Método inicializador de processso filho
 void proc_filho()
 {
 	printf("Processo FILHO %d iniciado\n",nfilho);
@@ -17,8 +18,10 @@ void proc_filho()
 
 void main()
 {
+	//Variaveis pid para os processos filhos
 	pid_t pid1, pid2, pid3, pid4;
 	nfilho+=1;
+	//Criação do primeiro processo filho
 	pid1 = fork();
 	
 	if(pid1>0)
@@ -26,17 +29,20 @@ void main()
 		printf("Processo PAI iniciado\n");
 		nfilho+=1;
 		wait(NULL);
+		//Criação do segundo processo filho
 		pid2 = fork();
 		if(pid2>0)
 		{
 			nfilho+=1;
 			wait(NULL);
+			//Criação do terceiro processo filho
 			pid3 = fork();
 			
 			if(pid3>0)
 			{
 				nfilho+=1;
 				wait(NULL);
+				//Criação do quarto processo filho
 				pid4 = fork();
 				
 				if(pid4>0)
