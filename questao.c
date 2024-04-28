8c881853de3f611617ff35b2ceb3ce9cbfaefa63
Eduardo Eifler <126223493+EiflerStorm@users.noreply.github.com>
2024-04-27 23:32:36 -0300

    Update2 questao.c
    Otimização da criação dos processos filhos com um 'for' loop, resultando em um código mais compacto e preparado para alterações.

--- questao.c
+++ questao.c
@@ -19,62 +19,23 @@ void proc_filho()
 void main()
 {
 	//Variaveis pid para os processos filhos
-	pid_t pid1, pid2, pid3, pid4;
-	nfilho+=1;
-	//Criação do primeiro processo filho
-	pid1 = fork();
+	pid_t pid;
 	
-	if(pid1>0)
+	for(int i=0;i<4;i+=1)
 	{
-		printf("Processo PAI iniciado\n");
 		nfilho+=1;
-		wait(NULL);
-		//Criação do segundo processo filho
-		pid2 = fork();
-		if(pid2>0)
+		
+		//Criação do primeiro processo filho
+		pid = fork();
+		if(pid>0)
 		{
-			nfilho+=1;
 			wait(NULL);
-			//Criação do terceiro processo filho
-			pid3 = fork();
-			
-			if(pid3>0)
-			{
-				nfilho+=1;
-				wait(NULL);
-				//Criação do quarto processo filho
-				pid4 = fork();
-				
-				if(pid4>0)
-				{
-					wait(NULL);
-					printf("Processo PAI encerrado\n");
-				} else if (pid4==0)
-				{
-					proc_filho();
-				} else
-				{
-					exit(-1);
-				}
-			} else if (pid3==0)
-			{
-				proc_filho();
-			} else
-			{
-				exit(-1);
-			}
-		} else if (pid2==0)
+		}else if
 		{
 			proc_filho();
-		} else
+		}else
 		{
 			exit(-1);
-		}	
-	} else if (pid1==0)
-	{
-		proc_filho();
-	} else
-	{
-		exit(-1);
+		}
 	}
 }
